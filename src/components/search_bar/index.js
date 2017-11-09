116d3d96496f85857cf6ee2dc58448e8d97a9459
Luis Manzo <manzosk3@gmail.com>
2017-11-08 21:22:32 -0800

    handles search bar change event to log input value

--- src/components/search_bar/index.js
+++ src/components/search_bar/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return (
-      <input type="text" />
+      <input type="text" onChange={event=>console.log(event.target.value)}/>
     )
   }
 }
