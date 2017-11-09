5c43d2fe13d7e8b54e259c0a32a2a9c811741cb8
Luis Manzo <manzosk3@gmail.com>
2017-11-08 22:53:32 -0800

    Revert "adds 'term' property o state of SearchBar component, sets state to update when input value changes"
    This reverts commit 1355b3c301987fd3a0314ce248d90467ff1f4d4a.

--- src/components/search_bar/index.js
+++ src/components/search_bar/index.js
@@ -1,20 +1,9 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { term: '' };
-  }
   render() {
     return (
-      <div>
-        <input
-          type="text"
-          value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value })} />
-        value of input: {this.state.term}
-      </div>
-
+      <input type="text" onChange={event=>console.log(event.target.value)}/>
     )
   }
 }
